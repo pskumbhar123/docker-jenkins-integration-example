ee3e0965bdbd1285533bafa85fa2c8b93b0dd36e
pskumbhar123 <pskumbhar123@gmail.com>
2020-01-15 22:14:08 +0530

    Add files via upload

---
+++ docker-jenkins-integration-example/src/test/java/com/javapsk/DockerJenkinsIntegrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
