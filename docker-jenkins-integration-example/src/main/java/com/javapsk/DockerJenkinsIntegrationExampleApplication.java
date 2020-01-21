74cd8ef1e17d0f4f4dd135a46a49349cdca1ceeb
pundalik <pskumbhar123@gmail.com>
2020-01-21 19:35:06 +0530

    4th commit

--- docker-jenkins-integration-example/src/main/java/com/javapsk/DockerJenkinsIntegrationExampleApplication.java
+++ docker-jenkins-integration-example/src/main/java/com/javapsk/DockerJenkinsIntegrationExampleApplication.java
@@ -8,7 +8,6 @@ public class DockerJenkinsIntegrationExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationExampleApplication.class, args);
-		System.out.println();
 	}
 
 }
